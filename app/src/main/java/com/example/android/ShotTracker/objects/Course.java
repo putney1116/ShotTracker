eaf2fb56761d679081f94ead7d1a7ed15fbd9d1e
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2014-11-29 00:41:48 -0700

    Add Course object

---
+++ app/src/main/java/com/example/android/ShotTracker/objects/Course.java
@@ -0,0 +1,61 @@
+package com.example.android.ShotTracker.objects;
+
+/**
+ * Class holding Course information
+ */
+public class Course {
+
+    private int mID;
+    private String mName;
+    private String mLocation;
+
+    /**
+     * Constructors
+     */
+    public Course() {}
+
+    public Course(int id, String name, String location) {
+        this.mID = id;
+        this.mName = name;
+        this.mLocation = location;
+    }
+
+    public Course(String name, String location) {
+        this.mName = name;
+        this.mLocation = location;
+    }
+
+    public Course(String name) {
+        this.mName = name;
+    }
+
+    /**
+     * Setters
+     */
+    public void setID(int id) {
+        this.mID = id;
+    }
+
+    public void setName(String name) {
+        this.mName = name;
+    }
+
+    public void setLocation(String location) {
+        this.mLocation = location;
+    }
+
+    /**
+     * Getters
+     */
+    public int getID() {
+        return this.mID;
+    }
+
+    public String getName() {
+        return this.mName;
+    }
+
+    public String getLocation() {
+        return this.mLocation;
+    }
+}
