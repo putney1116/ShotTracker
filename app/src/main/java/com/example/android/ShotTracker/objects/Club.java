586cbc053494011b70caf455849af3f9b5cbd375
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2014-11-29 00:03:30 -0700

    Add Club object

---
+++ app/src/main/java/com/example/android/ShotTracker/objects/Club.java
@@ -0,0 +1,57 @@
+package com.example.android.ShotTracker.objects;
+
+/**
+ * Class to hold Club information
+ */
+public class Club {
+
+    private int mID;
+    private String mClub;
+
+    /**
+     * Default Constructor
+     */
+    public Club() {}
+
+    /**
+     * Constructor
+     *
+     * @param id Unique Club ID
+     * @param club Club name
+     */
+    public Club(int id, String club) {
+        this.mID = id;
+        this.mClub = club;
+    }
+
+    /**
+     * Constructor
+     *
+     * @param club Club name
+     */
+    public Club(String club) {
+        this.mClub = club;
+    }
+
+    /**
+     * Setters
+     */
+    public void setID(int id) {
+        this.mID = id;
+    }
+
+    public void setClub(String club) {
+        this.mClub = club;
+    }
+
+    /**
+     * Getters
+     */
+    public int getID() {
+        return this.mID;
+    }
+
+    public String getClub() {
+        return this.mClub;
+    }
+}
