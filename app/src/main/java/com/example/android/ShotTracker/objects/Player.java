d14e4ddf24e7cd287b4e0f334ce83d0b9b60244f
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2014-11-28 23:58:43 -0700

    Add objects package containing Player class.

---
+++ app/src/main/java/com/example/android/ShotTracker/objects/Player.java
@@ -0,0 +1,81 @@
+package com.example.android.ShotTracker.objects;
+
+/**
+ * Class to hold Player information
+ */
+public class Player {
+
+    private int mID;
+    private String mName;
+    private int mHandicap;
+
+    /**
+     * Default constructor
+     */
+    public Player() {
+
+    }
+
+    /**
+     * Constructor
+     *
+     * @param id Unique ID number
+     * @param name Player Name
+     * @param handicap Player Handicap
+     */
+    public Player(int id, String name, int handicap) {
+        this.mID = id;
+        this.mName = name;
+        this.mHandicap = handicap;
+    }
+
+    /**
+     * Constructor
+     *
+     * @param name Player Name
+     * @param handicap Player Handicap
+     */
+    public Player(String name, int handicap) {
+        this.mName = name;
+        this.mHandicap = handicap;
+    }
+
+    /**
+     * Constructor
+     *
+     * @param name Player Name
+     */
+    public Player(String name) {
+        this.mName = name;
+    }
+
+    /**
+     * Set Methods
+     */
+    public void setID(int id) {
+        this.mID = id;
+    }
+
+    public void setName(String name) {
+        this.mName = name;
+    }
+
+    public void setHandicap(int handicap) {
+        this.mHandicap = handicap;
+    }
+
+    /**
+     * Get Methods
+     */
+    public int getID() {
+        return this.mID;
+    }
+
+    public String getName() {
+        return this.mName;
+    }
+
+    public int getHandicap() {
+        return this.mHandicap;
+    }
+}
