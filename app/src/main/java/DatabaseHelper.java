8f212586a3a13b23f6a7627334755a722ef783d6
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2014-12-10 19:39:02 -0700

    Push a comment to the log when Test Database is pressed from home screen.

---
+++ app/src/main/java/DatabaseHelper.java
@@ -0,0 +1,5 @@
+/**
+ * Created by damcglinchey on 11/28/14.
+ */
+public class DatabaseHelper {
+}
--- app/src/main/java/com/example/android/ShotTracker/TestDatabase.java
+++ app/src/main/java/com/example/android/ShotTracker/TestDatabase.java
@@ -11,17 +11,28 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.util.Log;
+
+import com.example.android.ShotTracker.db.DataBaseHelper;
+import com.example.android.ShotTracker.db.PlayerDAO;
 
 /**
- * Created by damcglinchey on 12/7/14.
+ * Activity for testing the database
  */
 public class TestDatabase extends Activity {
     private AlertDialog.Builder builder;
+
+    private static final String TAG = "TestDatabase";
 
     /** Called when the activity is first created */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+
+        //db = new DatabaseHelper(getApplicationContext());
+
+        Log.d(TAG, "A comment to the log.");
 
     }
 }
