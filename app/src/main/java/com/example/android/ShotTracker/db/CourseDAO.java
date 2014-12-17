1708fe126db3a6e162733868c9aaf00f363c9a36
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2014-12-16 20:47:32 -0700

    Add SubCourse CRUD methods.

--- app/src/main/java/com/example/android/ShotTracker/db/CourseDAO.java
+++ app/src/main/java/com/example/android/ShotTracker/db/CourseDAO.java
@@ -2,8 +2,13 @@ package com.example.android.ShotTracker.db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 
 import com.example.android.ShotTracker.objects.Course;
+import com.example.android.ShotTracker.objects.SubCourse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by damcglinchey on 12/16/14.
@@ -63,7 +68,7 @@ public class CourseDAO extends ShotTrackerDBDAO {
         return database.update(DataBaseHelper.COURSE_TABLE,
                 values,
                 WHERE_COURSEID_EQUALS,
-                new String[] {String.valueOf(course.getID())});
+                new String[]{String.valueOf(course.getID())});
     }
 
     /**
@@ -79,9 +84,128 @@ public class CourseDAO extends ShotTrackerDBDAO {
 
         return database.delete(DataBaseHelper.COURSE_TABLE,
                 WHERE_COURSEID_EQUALS,
-                new String[] {String.valueOf(course.getID())});
+                new String[]{String.valueOf(course.getID())});
     }
 
+    /**
+     * Get a list of all Course's in the DB
+     * @return A list of Course objects
+     */
+    public List<Course> readListofCourses() {
+        List<Course> courses = new ArrayList<Course>();
 
+        //\todo check nulls
 
+        Cursor cursor = database.query(DataBaseHelper.COURSE_TABLE,
+                new String[] {DataBaseHelper.COURSEID_COLUMN,
+                              DataBaseHelper.COURSENAME_COLUMN,
+                              DataBaseHelper.COURSELOCATION_COLUMN},
+                null, null, null, null, null);
+
+        while (cursor.moveToNext()) {
+            Course course = new Course();
+            course.setID(cursor.getInt(0));
+            course.setName(cursor.getString(1));
+            course.setLocation(cursor.getString(2));
+
+            courses.add(course);
+        }
+
+        return courses;
+    }
+
+    /**
+     * Add a subcourse to the DB
+     * @param subCourse SubCourse to be added. SubCourse.getCourseID() must be set
+     * @return
+     */
+    public long createSubCourse(SubCourse subCourse) {
+        ContentValues values = new ContentValues();
+
+        //\todo check that subCourse.getCourseID() is set before using it.
+
+        values.put(DataBaseHelper.COURSEID_COLUMN, subCourse.getCourseID());
+        values.put(DataBaseHelper.SUBCOURSENAME_COLUMN, subCourse.getName());
+
+        if (subCourse.getRating() > 0) {
+            values.put(DataBaseHelper.SUBCOURSERATING_COLUMN, subCourse.getRating());
+        }
+
+        return database.insert(DataBaseHelper.SUBCOURSE_TABLE, null, values);
+    }
+
+    /**
+     * Update SubCourse information in DB
+     * @param subCourse SubCourse to be updated. SubCourse.getID() must be set.
+     * @return
+     */
+    public long updateSubCourse(SubCourse subCourse) {
+        ContentValues values = new ContentValues();
+
+        if (subCourse.getID() < 0) {
+            throw new RuntimeException("SubCourse ID not set in CourseDAO.updateSubCourse()");
+        }
+
+        values.put(DataBaseHelper.COURSEID_COLUMN, subCourse.getCourseID());
+        values.put(DataBaseHelper.SUBCOURSENAME_COLUMN, subCourse.getName());
+
+        if (subCourse.getRating() > 0) {
+            values.put(DataBaseHelper.SUBCOURSERATING_COLUMN, subCourse.getRating());
+        }
+
+        return database.update(DataBaseHelper.SUBCOURSE_TABLE,
+                values,
+                WHERE_SUBCOURSEID_EQUALS,
+                new string[] {String.valueOf(subCourse.getID())});
+    }
+
+    /**
+     * Delete a SubCourse from the DB
+     * @param subCourse SubCourse to be deleted.
+     * @return
+     */
+    public long deleteSubCourse(SubCourse subCourse) {
+        if (subCourse.getID() < 0) {
+            throw new RuntimeException("SubCourse ID not set in CourseDAO.deleteSubCourse");
+        }
+
+        return database.delete(DataBaseHelper.SUBCOURSE_TABLE,
+                WHERE_SUBCOURSEID_EQUALS,
+                new String[] {String.valueOf(subCourse.getID())});
+    }
+
+    /**
+     * Get a list of SubCourses for a given Course
+     * @param course Course desired
+     * @return List of SubCourse objects
+     */
+    public List<SubCourse> readListofSubCourses(Course course) {
+        if (course.getID() < 0) {
+            throw new RuntimeException("Course ID not set in CourseDAO.readListofSubCourses()");
+        }
+
+        List<SubCourse> subCourses = new List<SubCourse>();
+
+        //\todo check for nulls
+
+        Cursor cursor = database.query(DataBaseHelper.SUBCOURSE_TABLE,
+                new String[] {DataBaseHelper.SUBCOURSEID_COLUMN,
+                DataBaseHelper.SUBCOURSENAME_COLUMN,
+                DataBaseHelper.SUBCOURSERATING_COLUMN},
+                WHERE_COURSEID_EQUALS,
+                new String[] {String.valueOf(course.getID())},
+                null, null, null);
+
+        while (cursor.moveToNext()) {
+            SubCourse subCourse = new SubCourse();
+            subCourse.setID(cursor.getInt(0));
+            subCourse.setCourseID(course.getID());
+            subCourse.setName(cursor.getString(1));
+            subCourse.setRating(cursor.getFloat(2));
+
+            subCourses.add(subCourse);
+        }
+
+        return subCourses;
+    }
 }
