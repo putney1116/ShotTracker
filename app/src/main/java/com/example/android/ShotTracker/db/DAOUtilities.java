9e19dfb483e3b7cb3f36f83e8bc2b05f0158a3b6
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2015-03-25 21:27:55 -0600

    Add a DAO utility class.

---
+++ app/src/main/java/com/example/android/ShotTracker/db/DAOUtilities.java
@@ -0,0 +1,19 @@
+package com.example.android.ShotTracker.db;
+
+import android.content.Context;
+
+/**
+ * Created by damcglinchey on 3/25/15.
+ */
+public class DAOUtilities {
+
+    Context context = null;
+
+    public DAOUtilities(Context cont){context = cont;}
+
+    // \todo Delete Round
+    // \todo Course from Round
+    // \todo Delete Player (and all player ID's)
+
+
+}
